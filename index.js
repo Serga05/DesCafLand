7c6e6ae432a00e49bcb3a3585ce9aec570744e29
Serga05 <88805832+Serga05@users.noreply.github.com>
2022-01-01 18:08:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function JSbuttonA() {
 	document.write('<title>DesCaf land(Игры)</title><body class="colorGrey"></body><a href="http://serga05.github.io/DesCafLand"><button class="BACKbutton"><-- Назад</button></a><link href="index.css" rel="stylesheet"><p><p class="Game">CS:GO</p><p class="Game">CupHead</p>')
 }
 function JSbuttonB() {
-	document.write('<body class="colorGrey"></body><a href="http://serga05.github.io/DesCafLand"><button class="BACKbutton"><-- Назад</button></a><link href="index.css" rel="stylesheet"><p><div class="block"></p><p class="PBlock"><img href="https://raw.githubusercontent.com/Serga05/photo1/main/infotab.png"><b>Что бы получить роль Админа пишите нам: Serga05#4147</div>')	
+	document.write('<body class="colorGrey"></body><a href="http://serga05.github.io/DesCafLand"><button class="BACKbutton"><-- Назад</button></a><link href="index.css" rel="stylesheet"><p><div class="block"></p><p class="PBlock"><img href="https://raw.githubusercontent.com/Serga05/photo1/main/infotab.png"><b style="font-family: none">Что бы получить роль Админа пишите нам: Serga05#4147</div></b>')	
 }
 function Sucsribe() {
 	document.write('<link href="index.css" rel="stylesheet"><body class="colorGrey"></body><h1>PKman</h1>Подписка PKman позволяет выкладывать<div> видео в сайт DesCaf Land и писать и<div> смотреть каналы по типу "YouTube Pro"<p><button class="Buy">Купить</button></p>')
