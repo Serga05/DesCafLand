ee9c6f2cd0da8fa5aeead2b5d10676c172a974f5
Serga05 <88805832+Serga05@users.noreply.github.com>
2022-01-01 17:11:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function JSbutton() {
+	document.write('<body class="colorGrey"></body><a href="http://serga05.github.io/DesCafLand"><button class="BACKbutton"><-- Назад</button></a><link href="index.css" rel="stylesheet"><p><title>DesCaf Land(Зачем это нужно?)</title>Этот сайт сделан для того что-бы ты мог посмотреть игры в нашем канале Discord</p>')
+}
+function JSbuttonA() {
+	document.write('<title>DesCaf land(Игры)</title><body class="colorGrey"></body><a href="http://serga05.github.io/DesCafLand"><button class="BACKbutton"><-- Назад</button></a><link href="index.css" rel="stylesheet"><p><p class="Game">CS:GO</p><p class="Game">CupHead</p>')
+}
+function JSbuttonB() {
+	document.write('<body class="colorGrey"></body><a href="http://serga05.github.io/DesCafLand"><button class="BACKbutton"><-- Назад</button></a><link href="index.css" rel="stylesheet"><p><div class="block"></p><p class="PBlock"><img href="https://raw.githubusercontent.com/Serga05/photo1/main/infotab.png"><b>Что бы получить роль Админа пишите нам: Serga05#4147</div>')	
+}
+function Sucsribe() {
+	document.write('<link href="index.css" rel="stylesheet"><body class="colorGrey"></body><h1>PKman</h1>Подписка PKman позволяет выкладывать<div> видео в сайт DesCaf Land и писать и<div> смотреть каналы по типу "YouTube Pro"<p><button class="Buy">Купить</button></p>')
+}
+function FriendButton() {
+	document.write('<a href="http://serga05.github.io/DesCafLand"><button class="BACKbutton"><-- Назад</button></a><P></p><link href="index.css" rel="stylesheet"><input disabled type="text" value="https://discord.gg/78CZVBqV6m" class="Friend"><button class="Copy" onclick="FriendCopy()">Копирывать</button><body class="colorGrey"></body>')
+}
+function FriendCopy() {
+	navigator.clipboard.writeText('https://discord.gg/78CZVBqV6m')
+}
+function Plus() {
+	document.write('<a href="http://serga05.github.io/DesCafLand"><button class="BACKbutton"><-- Назад</button></a><p></p>Что-бы посмотреть что делает Тема наведи на кнопку или текст с его именем<p><hr></hr><link href="index.css" rel="stylesheet"><body class="colorGrey"></body></p><button class="NeonButtonStyle">Neon</button><p></p><text class="Neon-text">Neon</text>')
+}
