9a2269ef996cac95f72c9c7584674be4ab82597d
Serga05 <88805832+Serga05@users.noreply.github.com>
2022-01-01 17:10:44 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function JSbutton() {
-	document.write('<a href="http://serga05.github.io/DesCafLand"><button class="BACKbutton"><-- Назад</button></a><link href="index.css" rel="stylesheet"><p><title>DesCaf Land(Зачем это нужно?)</title>Этот сайт сделан для того что-бы ты мог посмотреть игры в нашем канале Discord</p>')
-}
-function JSbuttonA() {
-	document.write('<a href="http://serga05.github.io/DesCafLand"><button class="BACKbutton"><-- Назад</button></a><link href="index.css" rel="stylesheet"><p><p class="Game">CS:GO</p><p class="Game">CupHead</p>')
-}
-function JSbuttonB() {
-	document.write('<a href="http://serga05.github.io/DesCafLand"><button class="BACKbutton"><-- Назад</button></a><link href="index.css" rel="stylesheet"><p><div class="block"></p><p class="PBlock"><b>Что бы получить роль Админа пишите нам: Serga05#4147</div>')	
-}
